3bfc1db047f5fd710a583bfa5f978fc63747f2b2
Jaku-BB <jaku.bb@outlook.com>
2024-05-28 21:45:56 +0200

    feat: add button

---
+++ client/src/app/components/button.tsx
@@ -0,0 +1,35 @@
+import { LoaderCircle } from "lucide-react";
+import type { ReactNode } from "react";
+
+interface ButtonProps {
+  children: ReactNode;
+  className?: string;
+  disabled?: boolean;
+  isLoading?: boolean;
+}
+
+export const Button = ({
+  children,
+  className,
+  disabled,
+  isLoading,
+}: ButtonProps) => {
+  return (
+    <button
+      className={[
+        "font-medium py-1.5 text-[15px] flex justify-center transition-colors hover:bg-indigo-400 bg-indigo-500 rounded-xl text-white",
+        (disabled || isLoading) && "opacity-50 cursor-not-allowed",
+        className,
+      ]
+        .filter(Boolean)
+        .join(" ")}
+      disabled={disabled || isLoading}
+    >
+      {isLoading ? (
+        <LoaderCircle size={22} className="animate-spin" />
+      ) : (
+        children
+      )}
+    </button>
+  );
+};
--- client/src/app/components/file-form.tsx
+++ client/src/app/components/file-form.tsx
@@ -1,8 +1,9 @@
+import { Button } from "~/app/components/button";
 import { ArrowUpFromLine } from "lucide-react";
 
 export const FileForm = () => {
   return (
-    <form className="max-w-xs w-full">
+    <form className="max-w-xs w-full flex flex-col gap-4">
       <label className="flex flex-col gap-3 relative border-dashed group has-[:focus]:border-indigo-400 has-[:focus]:bg-indigo-50 hover:border-indigo-400 hover:bg-indigo-50 transition-colors text-center py-4 px-8 border border-neutral-400 items-center rounded-xl">
         <ArrowUpFromLine className="text-neutral-900 transition-colors group-has-[:focus]:text-indigo-400 group-hover:text-indigo-400" />
         <span className="text-neutral-900 font-medium text-[15px]">
@@ -16,6 +17,7 @@ export const FileForm = () => {
           className="absolute top-0 left-0 h-full w-full opacity-0 cursor-pointer"
         />
       </label>
+      <Button>Share your file</Button>
     </form>
   );
 };
