e11dca498374881231fb94310cb026ac5b925147
Jaku-BB <jaku.bb@outlook.com>
2024-05-28 22:03:43 +0200

    feat: add visual hint if file is over input

--- client/src/app/components/file-form.tsx
+++ client/src/app/components/file-form.tsx
@@ -1,11 +1,29 @@
+"use client";
+
 import { Button } from "~/app/components/button";
+import { classNameMerge } from "~/app/utils/class-name-merge";
 import { ArrowUpFromLine } from "lucide-react";
+import { useState } from "react";
 
 export const FileForm = () => {
+  const [isFileOverInput, setIsFileOverInput] = useState(false);
+
   return (
     <form className="max-w-xs w-full flex flex-col gap-4">
-      <label className="flex flex-col gap-3 relative border-dashed group has-[:focus]:border-indigo-400 has-[:focus]:bg-indigo-50 hover:border-indigo-400 hover:bg-indigo-50 transition-colors text-center py-4 px-8 border border-neutral-400 items-center rounded-xl">
-        <ArrowUpFromLine className="text-neutral-900 transition-colors group-has-[:focus]:text-indigo-400 group-hover:text-indigo-400" />
+      <label
+        className={classNameMerge(
+          "flex flex-col gap-3 relative border-dashed group has-[:focus]:border-indigo-400 has-[:focus]:bg-indigo-50 hover:border-indigo-400 hover:bg-indigo-50 transition-colors text-center py-4 px-8 border border-neutral-400 items-center rounded-xl",
+          isFileOverInput && "border-indigo-400 bg-indigo-50",
+        )}
+        onDragEnter={() => setIsFileOverInput(true)}
+        onDragLeave={() => setIsFileOverInput(false)}
+      >
+        <ArrowUpFromLine
+          className={classNameMerge(
+            "transition-colors group-has-[:focus]:text-indigo-400 group-hover:text-indigo-400",
+            isFileOverInput ? "text-indigo-400" : "text-neutral-900",
+          )}
+        />
         <span className="text-neutral-900 font-medium text-[15px]">
           Drop your file or click here
         </span>
