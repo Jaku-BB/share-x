0318e49dd76b78ccc93518f718a91a7ea41e7bc0
LeszekSarepski <rgolyszny@gmail.com>
2024-05-30 19:28:44 +0200

    feat: add /file POST method

--- server/Controllers/FileController.cs
+++ server/Controllers/FileController.cs
@@ -1,6 +1,13 @@
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 
 namespace server.Controllers;
+
+public class FileMap
+{
+    public string OriginalFileName { get; set; } = string.Empty;
+    public string FileId { get; set; } = string.Empty;
+}
 
 [Route("file")]
 public class FileController : Controller
@@ -8,7 +15,38 @@ public class FileController : Controller
     [HttpPost]
     public IActionResult SaveFile(IFormFile file)
     {
-        return Ok();
+        if (file.Length > 1024 * 1024 * 16) return StatusCode(413);
+
+        string fileId = Guid.NewGuid().ToString();
+        string extension = Path.GetExtension(file.FileName);
+
+        string storagePath = Path.Combine(Directory.GetCurrentDirectory(), "Storage");
+
+        if (!Directory.Exists(storagePath)) Directory.CreateDirectory(storagePath);
+
+        using FileStream stream = new(Path.Combine(storagePath, fileId + extension), FileMode.Create);
+        file.CopyTo(stream);
+
+        string fileMapPath = Path.Combine(storagePath, "FileMap.json");
+
+        if (!System.IO.File.Exists(fileMapPath)) System.IO.File.WriteAllText(fileMapPath, "[]");
+
+        string fileMapJson = System.IO.File.ReadAllText(fileMapPath);
+
+        var fileMap = JsonSerializer.Deserialize<List<FileMap>>(fileMapJson);
+
+        fileMap?.Add(new FileMap
+        {
+            OriginalFileName = file.FileName,
+            FileId = fileId
+        });
+
+        System.IO.File.WriteAllText(fileMapPath, JsonSerializer.Serialize(fileMap));
+
+        return Ok(new
+        {
+            fileId
+        });
     }
 
     [HttpGet("{id}")]
