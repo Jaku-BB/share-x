af8d41de321363dd61e29d1f3aea6dc1b049ae52
Jaku-BB <jaku.bb@outlook.com>
2024-06-03 17:41:20 +0200

    refactor: extract route logic to service

--- server/Controllers/FileController.cs
+++ server/Controllers/FileController.cs
@@ -1,77 +1,35 @@
-using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
+using server.Services;
 
 namespace server.Controllers;
-
-public class FileMap
-{
-    public string OriginalFileName { get; set; } = string.Empty;
-    public string FileId { get; set; } = string.Empty;
-}
 
 [Route("file")]
 public class FileController : Controller
 {
+    private const int MaximumFileSize = 1024 * 1024 * 16;
+    private FileStorageService _fileStorageService = new();
+
     [HttpPost]
     public IActionResult SaveFile(IFormFile file)
     {
-        if (file.Length > 1024 * 1024 * 16) return StatusCode(413);
+        if (file.Length > MaximumFileSize)
+            return new JsonResult(new { message = "File is too large! " })
+            {
+                StatusCode = 413
+            };
 
-        string fileId = Guid.NewGuid().ToString();
-        string extension = Path.GetExtension(file.FileName);
+        string fileId = _fileStorageService.StoreFile(file);
 
-        string storagePath = Path.Combine(Directory.GetCurrentDirectory(), "Storage");
-
-        if (!Directory.Exists(storagePath)) Directory.CreateDirectory(storagePath);
-
-        using FileStream stream = new(Path.Combine(storagePath, fileId + extension), FileMode.Create);
-        file.CopyTo(stream);
-
-        string fileMapPath = Path.Combine(storagePath, "FileMap.json");
-
-        if (!System.IO.File.Exists(fileMapPath)) System.IO.File.WriteAllText(fileMapPath, "[]");
-
-        string fileMapJson = System.IO.File.ReadAllText(fileMapPath);
-
-        var fileMap = JsonSerializer.Deserialize<List<FileMap>>(fileMapJson);
-
-        fileMap?.Add(new FileMap
-        {
-            OriginalFileName = file.FileName,
-            FileId = fileId
-        });
-
-        System.IO.File.WriteAllText(fileMapPath, JsonSerializer.Serialize(fileMap));
-
-        return Ok(new
-        {
-            fileId
-        });
+        return Created($"/file/{fileId}", new { fileId });
     }
 
     [HttpGet("{id}")]
     public IActionResult GetFile(string id)
     {
-        string storagePath = Path.Combine(Directory.GetCurrentDirectory(), "Storage");
+        string filePath = _fileStorageService.GetFilePath(id);
 
-        string fileMapPath = Path.Combine(storagePath, "FileMap.json");
+        if (string.IsNullOrEmpty(filePath)) return NotFound();
 
-        if (!System.IO.File.Exists(fileMapPath)) return NotFound();
-
-        string fileMapJson = System.IO.File.ReadAllText(fileMapPath);
-
-        var fileMap = JsonSerializer.Deserialize<List<FileMap>>(fileMapJson);
-
-        var file = fileMap?.FirstOrDefault(x => x.FileId == id);
-
-        if (file == null) return NotFound();
-
-        string extension = Path.GetExtension(file.OriginalFileName);
-
-        string filePath = Path.Combine(storagePath, id + extension);
-
-        if (!System.IO.File.Exists(filePath)) return NotFound();
-
-        return PhysicalFile(filePath, "application/octet-stream", file.OriginalFileName);
+        return PhysicalFile(filePath, "application/octet-stream");
     }
 }
---
+++ server/Services/FileStorageService.cs
@@ -0,0 +1,61 @@
+using System.Text.Json;
+
+namespace server.Services;
+
+public class FileStorageService
+{
+    private static string _fileStoragePath = Path.Combine(Directory.GetCurrentDirectory(), "Storage");
+    private string _fileMapPath = Path.Combine(_fileStoragePath, "FileMap.json");
+
+    private class FileMapEntity
+    {
+        public string FileId { get; set; } = string.Empty;
+        public string OriginalFileName { get; set; } = string.Empty;
+    }
+
+    public FileStorageService()
+    {
+        if (!Directory.Exists(_fileStoragePath)) Directory.CreateDirectory(_fileStoragePath);
+        if (!File.Exists(_fileMapPath)) File.WriteAllText(_fileMapPath, "[]");
+    }
+
+    private List<FileMapEntity> GetFileMap()
+    {
+        string rawFileMapContent = File.ReadAllText(_fileMapPath);
+        return JsonSerializer.Deserialize<List<FileMapEntity>>(rawFileMapContent) ?? [];
+    }
+
+    public string StoreFile(IFormFile file)
+    {
+        string fileId = "ID";
+        string extension = Path.GetExtension(file.FileName);
+
+        using FileStream stream = new(Path.Combine(_fileStoragePath, fileId + extension), FileMode.Create);
+        file.CopyTo(stream);
+
+        var fileMap = GetFileMap();
+
+        fileMap.Add(new FileMapEntity
+        {
+            FileId = fileId,
+            OriginalFileName = file.FileName
+        });
+
+        File.WriteAllText(_fileMapPath, JsonSerializer.Serialize(fileMap));
+
+        return fileId;
+    }
+
+    public string GetFilePath(string fileId)
+    {
+        var fileMap = GetFileMap();
+        var file = fileMap.FirstOrDefault(file => file.FileId == fileId);
+
+        if (file == null) return string.Empty;
+
+        string extension = Path.GetExtension(file.OriginalFileName);
+        string filePath = Path.Combine(_fileStoragePath, fileId + extension);
+
+        return File.Exists(filePath) ? filePath : string.Empty;
+    }
+}
