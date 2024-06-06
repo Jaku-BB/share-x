958e4e0b83a4ba46e6413caf5764a990e0c0b540
Jaku-BB <jaku.bb@outlook.com>
2024-06-06 22:14:21 +0200

    feat: add global `not-found.tsx`

---
+++ client/src/app/not-found.tsx
@@ -0,0 +1,11 @@
+const NotFound = () => {
+  return (
+    <div className="flex items-start justify-center">
+      <h2 className="text-center text-2xl font-medium">
+        The requested resource does not exist! 😢
+      </h2>
+    </div>
+  );
+};
+
+export default NotFound;
