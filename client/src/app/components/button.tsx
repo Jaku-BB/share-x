34d352a5a70b5366ead9cfb4cf4a294340dffdfc
Jaku-BB <jaku.bb@outlook.com>
2024-06-06 22:13:24 +0200

    feat: add file page with existence validation

--- client/src/app/components/button.tsx
+++ client/src/app/components/button.tsx
@@ -9,6 +9,9 @@ interface ButtonProps {
   isLoading?: boolean;
 }
 
+export const baseButtonClassName =
+  "font-medium px-3 py-1.5 text-[15px] flex justify-center focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-blue-300 transition-colors hover:bg-indigo-400 bg-indigo-500 rounded-xl text-white";
+
 export const Button = ({
   children,
   className,
@@ -18,7 +21,7 @@ export const Button = ({
   return (
     <button
       className={classNameMerge(
-        "font-medium py-1.5 text-[15px] flex justify-center focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-blue-300 transition-colors hover:bg-indigo-400 bg-indigo-500 rounded-xl text-white",
+        baseButtonClassName,
         (disabled || isLoading) && "opacity-50 cursor-not-allowed",
         className,
       )}
---
+++ client/src/app/file/[id]/page.tsx
@@ -0,0 +1,43 @@
+import { baseButtonClassName } from "~/app/components/button";
+import { classNameMerge } from "~/app/utils/class-name-merge";
+import { FileDown } from "lucide-react";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL as string;
+
+const checkFileExistence = async (id: string) => {
+  const fileServerRoute = BASE_URL + "/file/" + id;
+
+  const response = await fetch(fileServerRoute, {
+    method: "HEAD",
+  });
+
+  return response.ok ? fileServerRoute : null;
+};
+
+const Page = async ({ params: { id } }: { params: { id: string } }) => {
+  const fileServerRoute = await checkFileExistence(id);
+
+  if (!fileServerRoute) {
+    return notFound();
+  }
+
+  return (
+    <main className="flex justify-start gap-8 flex-col items-center">
+      <p className="text-neutral-900 text-center">
+        File is up and ready for you!
+      </p>
+      <Link
+        href={fileServerRoute}
+        className={classNameMerge(baseButtonClassName, "gap-1.5")}
+        download
+      >
+        <FileDown aria-hidden size={22} />
+        Get File
+      </Link>
+    </main>
+  );
+};
+
+export default Page;
